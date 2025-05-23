826e2a170baeb1294f03b4c271608f6a77576f9e
JohnMarit <thuchabraham42@gmail.com>
2025-05-23 12:40:52 +0200

    map 4

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
-const API_URL =
-  import.meta.env.VITE_API_URL ||
-  (window.location.hostname !== 'localhost'
-    ? 'https://community-guard-2525c539a22c.herokuapp.com/api'
-    : 'http://localhost:3001/api');
+const API_URL = 'https://community-guard-2525c539a22c.herokuapp.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
